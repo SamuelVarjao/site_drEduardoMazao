3366aaa4d36acc4db09a8c92bf310c1fccf7c47a
samuelv2umc <11221103351@alunos.umc.br>
2025-12-21 16:24:43 -0300

    alterando vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        base: 'https://github.com/SamuelVarjao/site_drEduardoMazao',
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
 })
