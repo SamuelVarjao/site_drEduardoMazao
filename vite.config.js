1fa20ec302e89ccbfa7b043627b2f22f682b1de1
samuelv2umc <11221103351@alunos.umc.br>
2026-02-09 17:37:29 -0300

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/'
+  base: mode === 'production'
+    ? '/site_drEduardoMazao/'
+    : '/',
 }))
